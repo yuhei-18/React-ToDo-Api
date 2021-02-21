874e6210e346076ee26ec345c99894b60611902f
yuhei <kasaiyuhei8@gmail.com>
2021-02-22 02:18:55 +0900

    ref: rails routes で余分なルートを表示されないようにする

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module ReactTodo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    initializer(:remove_action_mailbox_and_activestorage_routes, after: :add_routing_paths) { |app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
+      app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/ }
+    }
+
     config.generators do |g|
       g.test_framework :rspec
     end
