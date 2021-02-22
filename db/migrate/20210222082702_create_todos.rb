4e4994239141dd0993fce96c707ef3ae5a09f2ba
yuhei <kasaiyuhei8@gmail.com>
2021-02-22 21:44:28 +0900

    ref: リネーム

--- db/migrate/20210222082702_create_todos.rb
+++ db/migrate/20210222082702_create_todos.rb
@@ -1,8 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
-      t.string :title
-      t.string :text
+      t.string "title", null: false
+      t.text "content", null: false
 
       t.timestamps
     end
