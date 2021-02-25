f2517c339c53e04b56101ba84a64c8de63388bfa
yuhei <kasaiyuhei8@gmail.com>
2021-02-25 22:12:31 +0900

    ref: Faker の設定を2日前から2日後に修正する

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     title { Faker::Lorem.paragraph }
     content { Faker::Lorem.paragraph }
     priority { rand(0..3) }
-    due_date { Faker::Date.between(from: 2.days.ago, to: Date.today) }
+    due_date { Faker::Date.between(from: 2.days.since, to: Date.today) }
   end
 end
