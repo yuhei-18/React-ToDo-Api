bc8767280b0158bb94e070783f3db33e24e973d7
yuhei <kasaiyuhei8@gmail.com>
2021-02-25 09:37:07 +0900

    test: カラム追加に伴ったテストの修正

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :todo do
     title { Faker::Lorem.paragraph }
     content { Faker::Lorem.paragraph }
+    priority { rand(0..3) }
+    due_date { Faker::Date.between(from: 2.days.ago, to: Date.today) }
   end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe Todo, type: :model do
   context 'Create' do
     let(:name) { 'simple name' }
     let(:param) { FactoryBot.create :todo }
-    let(:todo) { Todo.create(:title => param.title, :content => param.content) }
+    let(:todo) { Todo.create(:title => param.title, :content => param.content, :priority => param.priority, :due_date => param.due_date) }
 
     it 'データが正しく作成されている事' do
       expect(todo.title).to eq param.title
       expect(todo.content).to eq param.content
+      expect(todo.priority).to eq param.priority
+      expect(todo.due_date).to eq param.due_date
+      expect(todo.is_done).to eq false
     end
   end
 end
